a9e191da459ae974469951fc0332cd75469c186d
laragalvao7 <larafantuci@hotmail.com>
2025-06-26 08:54:54 -0300

    ok

---
+++ script.js
@@ -0,0 +1,10 @@
+function inserirNome() {
+
+    let nomeUsuario = prompt("Qual o seu nome");
+
+    let elemento = document.querySelector("#nome-usuario");
+
+    elemento.textContent = nomeUsuario;
+
+}
+inserirNome();
