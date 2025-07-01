7e1b4e668925e6785dddabe63809012cb88e5916
laragalvao7 <larafantuci@hotmail.com>
2025-07-01 14:28:46 -0300

    ok

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ function inserirNome() {
 
     let nomeUsuario = prompt("Qual o seu nome");
 
-    let elemento = 
-    document.querySelector("#nome-usuario");
-
+    let elemento = document.querySelector("#nome-usuario");
+   console.log(elemento);
     elemento.textContent = nomeUsuario;
 }
 inserirNome();
+let linguagens = ["JavaScript","Python","C"];​
+console.log(linguagens[0]) // “JavaScript”
+console.log(linguagens[1]) // “Python”
+console.log(linguagens[2]) // “C”
