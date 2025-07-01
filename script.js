fd54d9dfcf467ed0c92f4e26132b704c4321c36a
laragalvao7 <larafantuci@hotmail.com>
2025-07-01 14:44:28 -0300

    ok

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ function inserirNome() {
     elemento.textContent = nomeUsuario;
 }
 inserirNome();
-let linguagens = ["JavaScript","Python","C++"];​
-console.log(linguagens[0]) // “JavaScript”
-console.log(linguagens[1]) // “Python”
-console.log(linguagens[2]) // “C++”
+let linguagens = ["JavaScript", "Python","C++"]
+console.log (linguagens [0])
+console.log (linguagens [1])
+console.log (linguagens [2])
+
+const lista = document.querySelector ("#lista");
+lista.textContent = linguagens [0];
+lista.textContent = linguagens [1];
+lista.textContent = linguagens [2];
